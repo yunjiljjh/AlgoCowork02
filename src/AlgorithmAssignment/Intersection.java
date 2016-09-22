fcb31cc41f5cb9ba0d01d19ec5bd0fccb72830a1
yunjiljjh <yunjiljjh@naver.com>
2016-09-22 19:15:03 +0900

    .
    .

--- src/AlgorithmAssignment/Intersection.java
+++ src/AlgorithmAssignment/Intersection.java
@@ -1,5 +1,33 @@
 package AlgorithmAssignment;
 
-public class Intersection {
+import java.util.Vector;
+import java.awt.Point;
 
+public class Intersection {
+	
+	Vector<DoublePoint> v =null;
+	
+	public Intersection(){
+		 v = new Vector<DoublePoint>();
+	}
+
+	public void add(double x, double y){
+		DoublePoint p = new DoublePoint(x,y);
+		v.add(p);
+	}
+	public double[] getOne(int i){ //i번째 point를 [0]=x, [1]=y로  리턴
+		DoublePoint p = v.elementAt(i);
+		double[] darr = new double[2];
+		darr[0]=p.getX();
+		darr[1]=p.getY();
+		return darr;
+	}
+	public void popOne(int i){ //i+1번째 point 객체 삭제
+		v.remove(i);
+	}
+	
+	public void clear(){ //clear out the vecter (erase all)
+		v.clear();
+	}
+	
 }
