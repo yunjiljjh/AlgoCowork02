3bd585af60e3ba057e69f3c9e4eb200f0dfe201d
EuniceNam <caellime@gmail.com>
2016-09-22 18:05:06 +0900

    still working on lineSeg and InputReader

--- src/AlgorithmAssignment/lineSegment.java
+++ src/AlgorithmAssignment/lineSegment.java
@@ -7,6 +7,12 @@ public class lineSegment {
 	public double y1;
 	public double y2;
 	
+	public lineSegment(double x1, double y1, double x2, double y2)
+	{
+		this.x1 = x1; this.y1 = y1; 
+		this.x2 = x2; this.y2 = y2;
+	}
+	
 	public double yVal(double X) // return y value of given X of this line
 	{
 		return ((y1-y2)/(x1-x2))*(X-x1) + y1; // line equation
