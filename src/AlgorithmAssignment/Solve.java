e34ee44870685242c8641adc707215de8aee6a8b
EuniceNam <caellime@gmail.com>
2016-09-24 18:27:29 +0900

    .

--- src/AlgorithmAssignment/Solve.java
+++ src/AlgorithmAssignment/Solve.java
@@ -1,5 +1,6 @@
 package AlgorithmAssignment;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Vector;
 
@@ -13,19 +14,29 @@ public class Solve {
 	private lineSegment segments[]; // all segments of a test case
 	public String result = "";
 	
-	Solve(int numOfTestCase, lineSegment[][] segsOfTestCases, DoublePoint[] pointsOnAxis){
+	Solve(int numOfTestCase, lineSegment[][] segsOfTestCases){
 		//for a test case
 		for (int i=0; i < numOfTestCase; i++){
 			segments = segsOfTestCases[i];
 			
 			Arrays.sort(segments); //need to check if this works
+			//for sort test, print segsOfTestCases	
+			System.out.println("sort test");
+				System.out.printf("segsOfTestCases[%d]\n", i);
+				int z = Array.getLength(segsOfTestCases[i]);
+				for(int k=0; k < z/2; k++){
+				System.out.printf("segments[%d]: (%.2f, %.2f), (%.2f, %.2f) ", 2*k, segsOfTestCases[i][2*k].x1, segsOfTestCases[i][2*k].y1, segsOfTestCases[i][2*k].x2,segsOfTestCases[i][2*k].y2);
+				System.out.printf("segments[%d]: (%.2f, %.2f), (%.2f, %.2f)\n", 2*k+1, segsOfTestCases[i][2*k+1].x1, segsOfTestCases[i][2*k+1].y1, segsOfTestCases[i][2*k+1].x2,segsOfTestCases[i][2*k+1].y2);
+				}
+				System.out.println("a testcase ends");
+
 			Intersection I = new Intersection(segments);
 //			Vector<DoublePoint> v = I.vReturn();
 			//check if any line segment exists above the points of intersection and bottom
 			//pointsOnAxis[2*(building#)] is (segsOfTestCases[testcase#][2*(building#)].x1, 0) and [2*k+1] is (segsOfTestCases[testcase#][2*(building#)+1].x2, 0)					
 
 /*for test			
-			Checker chk = Checker(I.vRet(), pointsOnAxis); //
+			Checker chk = Checker(I.vRet()); //
 			Arrays.sort(chk.points); //result should be sorted
 			//or Collections.sort(chk.points) for vector
 			
