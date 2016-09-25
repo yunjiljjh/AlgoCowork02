4aae5b1453166a83cde90703a14d393aa41d31df
yunjiljjh <yunjiljjh@naver.com>
2016-09-25 09:23:16 +0900

    Revert "."
    This reverts commit 09a30fc6ba4e4c562caea49296c7650db9c6df01.

--- src/AlgorithmAssignment/Main.java
+++ src/AlgorithmAssignment/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		InputReader2 reader=new InputReader2("C:\\hw2\\input.txt");
+		InputReader reader=new InputReader("C:\\hw2\\input.txt");
 		Solve slv = new Solve(reader.numOfTestcase, reader.segsOfTestCases, reader.pointsOnAxis);
 		//which include loops of
 		//		Intersection();
